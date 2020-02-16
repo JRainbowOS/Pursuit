7794a2b7985d4423ee855663fa3de06f9b150b25
JRainbowOS <jacob.rainbow@gmail.com>
2020-02-16 10:56:58 +0000

    mouse can now spiral

--- pursuit/generator.py
+++ pursuit/generator.py
@@ -6,11 +6,26 @@ def direction_generator(start_x, start_y, dx, dy):
         yield direction
         direction += np.array([dx, dy])
 
-
+def angle_generator(start_angle, increment):
+    angle = start_angle
+    while True:
+        yield angle % (2 * np.pi)
+        angle += increment 
+        
 start_x, start_y = 0, 0
-dx, dy = 1, 0
+dx, dy = 1, 2
 
-g = direction_generator(start_x, start_y, dx, dy)
+# g = direction_generator(start_x, start_y, dx, dy)
+g = angle_generator(0, 1)
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
+print(next(g))
 print(next(g))
 print(next(g))
 print(next(g))
--- pursuit/mouse.py
+++ pursuit/mouse.py
@@ -1,28 +1,50 @@
 import numpy as np
+import matplotlib.pyplot as plt 
 
 from pursued import Pursued
 
 class Mouse(Pursued):
 
     SPEED = 1
-    INITIAL_DIRECTION_VECTOR = np.array([1, 0])
+    SPEED_INC = 0.1
+    INITIAL_ANGLE = 0
+    ANGLE_INC = 0.1
 
     def __init__(self, current_position_x, current_position_y):
         self.current_position_x = current_position_x
         self.current_position_y = current_position_y
-        self.speed = Mouse.SPEED
+        self.speed = self.speed_generator(Mouse.SPEED, Mouse.SPEED_INC)
+        self.angle = self.angle_generator(Mouse.INITIAL_ANGLE, Mouse.ANGLE_INC)
 
     def evolve(self):
         super().evolve()
         print(f'Time step is {self._dt}')
-        direction_vector = next(self.direction_generator(1, 0))
-        print(direction_vector)
+        angle = next(self.angle)
+        speed = next(self.speed)
+
+        dx, dy = self.get_movement_vector(angle, speed)
+        self.current_position_x += dx
+        self.current_position_y += dy        
 
     @staticmethod
-    def direction_generator(dx, dy):
-        direction_vector = np.array([0, 1])
+    def angle_generator(initial_angle, increment):
+        angle = initial_angle
         while True:
-            yield direction_vector + np.array([dx, dy])
+            yield angle % (2 * np.pi)
+            angle += increment
+
+    @staticmethod
+    def speed_generator(initial_speed, increment):
+        speed = initial_speed
+        while True:
+            yield speed 
+            speed += increment
+
+    @staticmethod
+    def get_movement_vector(angle, speed):
+        dx = np.sin(angle)
+        dy = np.cos(angle)
+        return [dx * speed, dy * speed]
 
 
 
@@ -31,10 +53,20 @@ def main():
     START_POSITION = [0,0]
     PURSUED_SPEED = 1
 
-    stuart = Mouse(*START_POSITION)
-    stuart.evolve()
-    stuart.evolve()
-    # print(stuart)
+    rambil = Mouse(*START_POSITION)
+    
+    s = []
+    t = []
+    for _ in range(100):
+        rambil.evolve()
+        x = rambil.current_position_x
+        y = rambil.current_position_y
+        s.append(x)
+        t.append(y)
+        print(x, y)
+
+    plt.plot(s, t)
+    plt.show()
 
 if __name__ == '__main__':
     main()
